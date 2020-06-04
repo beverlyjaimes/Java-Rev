ffd8ceb396e5b5f74eb9e9ac8ffa809f74e0ce84
Beverly Jaimes Puente <bpuente@appddictionstudio.com>
2020-06-04 14:50:45 -0500

    added age restriction

--- TreeStory.java
+++ TreeStory.java
@@ -1,24 +1,33 @@
 import java.io.Console;
+import java.util.Scanner;
+
  
 public class TreeStory {
     
     public static void main(String[] args) {
         Console console = System.console();
-        /*  Some terms:
-            noun - Person, place or thing
-            verb - An action
-            adjective - A description used to modify or describe a noun
-            Enter your amazing code here!
-        */
-    String name = console.readLine("Enter a name:  ");
-    String adjective = console.readLine("Enter an adjective:  ");
-    String noun = console.readLine("Enter a noun:  ");
-    String adverb = console.readLine("Enter an adverb:  ");
-    String verb = console.readLine("Enter a verb ending with -ing:  ");
-      
-      console.printf("Your TreeStory:\n---------------------\n");
-      console.printf("%s is a %s %s. ", name, adjective, noun);
-      console.printf("They are always %s %s.\n", adverb, verb);
+        Scanner scan = new Scanner(System.in);
+        /*
+         * Some terms: noun - Person, place or thing verb - An action adjective - A
+         * description used to modify or describe a noun Enter your amazing code here!
+         */
+    System.out.print("How old are you?");
+    int age = scan.nextInt();
+    System.out.print(age);
+    if (age > 13 ) {
+        String name = console.readLine("Enter a name:  ");
+        String adjective = console.readLine("Enter an adjective:  ");
+        String noun = console.readLine("Enter a noun:  ");
+        String adverb = console.readLine("Enter an adverb:  ");
+        String verb = console.readLine("Enter a verb ending with -ing:  ");
+          
+          console.printf("Your TreeStory:\n---------------------\n");
+          console.printf("%s is a %s %s. ", name, adjective, noun);
+          console.printf("They are always %s %s.\n", adverb, verb);
+    } else {
+        console.printf("You are too young!");
+    }
+   
     }
     
 }
