97855a275fa12e1511230901b04794a4576066ae
Beverly Jaimes Puente <bpuente@appddictionstudio.com>
2020-06-15 08:34:29 -0500

    updated prompter to display progress

--- HangmanGame/Hangman.java
+++ HangmanGame/Hangman.java
@@ -4,11 +4,13 @@ public class Hangman {
     public static void main(String[] args) {
         Game game = new Game("treehouse");
         Prompter prompter = new Prompter(game);
+        prompter.displayProgress();
         boolean isHit = prompter.promtForGuess();
         if (isHit) {
             System.out.println("We've got a hit!");
         } else {
             System.out.println("Oops missed!");
         }
+        prompter.displayProgress();
     }
 }
