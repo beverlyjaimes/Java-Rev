08eee0261e5647792444b5c4226e96308cacdd5b
Beverly Jaimes Puente <bpuente@appddictionstudio.com>
2020-06-15 10:26:11 -0500

    mvp finished

--- HangmanGame/Hangman.java
+++ HangmanGame/Hangman.java
@@ -4,13 +4,20 @@ public class Hangman {
     public static void main(String[] args) {
         Game game = new Game("treehouse");
         Prompter prompter = new Prompter(game);
-        prompter.displayProgress();
-        boolean isHit = prompter.promtForGuess();
-        if (isHit) {
-            System.out.println("We've got a hit!");
-        } else {
-            System.out.println("Oops missed!");
+
+        while (game.getRemainingTries() > 0 ) {
+            prompter.displayProgress();
+            prompter.promtForGuess();
         }
-        prompter.displayProgress();
+       
+
+
+        // boolean isHit = prompter.promtForGuess();
+        // if (isHit) {
+        //     System.out.println("We've got a hit!");
+        // } else {
+        //     System.out.println("Oops missed!");
+        // }
+        // prompter.displayProgress();
     }
 }
